4ba4867ff220e0f4500cb622849a176305bc3c26
rizkipra649 <rizkipra649@gmail.com>
2024-12-13 07:33:45 +0000

    asfaf

--- js/script.js
+++ js/script.js
@@ -49,3 +49,27 @@ button.addEventListener("click", () => {
 });
 
 //Backend tombol mulai belajar
+
+
+
+//testimonial
+
+
+      function showSlide(index) {
+        const slider = document.querySelector('.testimonials-slider');
+        const totalSlides = slider.children.length;
+        if (index < 0) currentIndex = totalSlides - 1;
+        else if (index >= totalSlides) currentIndex = 0;
+        else currentIndex = index;
+        const offset = -currentIndex * 100;
+        slider.style.transform = `translateX(${offset}%)`;
+      }
+
+      function prevSlide() {
+        showSlide(currentIndex - 1);
+      }
+
+      function nextSlide() {
+        showSlide(currentIndex + 1);
+      }
+//testimonial
